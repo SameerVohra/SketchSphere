1e965dcd720aa3d8870c8b41615d8c0b02e54c76
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-06-18 00:33:31 +0530

    fixed the error with CORS

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "/login" || "/",
         element: <Login />,
       },
       {
